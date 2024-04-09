b71cf4669b60b631aeacfdbd3db217ea8d6956c5
wltjsdud0782 <wltjsdud0782@gmail.com>
2024-04-09 12:48:55 +0900

    그래보이는 그래프 컬럼명 조금 수정

--- src/main/resources/static/js/home.js
+++ src/main/resources/static/js/home.js
@@ -2,7 +2,7 @@
 new Chart(document.querySelector('#line-chart'), {
     type: 'line',
     data: {
-        labels: ['a', 'b', 'c', 'd', 'e'],
+        labels: ['9시', '11시', '13시', '15시', '17시'],
         datasets: [
             {
                 data: [10.5, 11.5, 15.7, 8.5, 13.0],
@@ -25,8 +25,8 @@ new Chart(document.querySelector('#line-chart'), {
         },
         scales: {
             y: {
-                min: 0,
-                max: 20
+                min: -20,
+                max: 40
             }
         }
     }
@@ -36,30 +36,30 @@ new Chart(document.querySelector('#line-chart'), {
 new Chart(document.querySelector('#mixed-chart'), {
     type: 'bar',
     data: {
-        labels: ["1900", "1950", "1999", "2050"],
+        labels: ["1월", "2월", "3월", "현재"],
         datasets: [{
             label: "실시간 온도",
             type: "line",
             borderColor: "#8e5ea2",
-            data: [408, 547, 675, 734],
+            data: [15,20,23,28],
             fill: false
         }, {
             label: "실시간 습도",
             type: "line",
             borderColor: "#3e95cd",
-            data: [133, 221, 783, 2478],
+            data: [30,15,15,20],
             fill: false
         }, {
             label: "평균 온도",
             type: "bar",
             backgroundColor: "rgba(0,0,0,0.2)",
-            data: [408, 547, 675, 734],
+            data: [18,24,21,30],
         }, {
             label: "평균 습도",
             type: "bar",
             backgroundColor: "rgba(0,0,0,0.2)",
             backgroundColorHover: "#3e95cd",
-            data: [133, 221, 783, 2478]
+            data: [28,21,21,25]
         }
         ]
     },
@@ -81,7 +81,7 @@ new Chart(document.querySelector('#pie-chart'), {
         datasets: [{
             label: "미세먼지 등급",
             backgroundColor: ["#3e95cd", "#8e5ea2", "#3cba9f", "#e8c3b9", "#c45850"],
-            data: [10, 20, 50, 15, 5]
+            data: [10, 25, 40, 20, 5]
         }]
     },
     options: {
